bccf945f1e38db4e9be1ed9bfb0205dc30e5dd2f
Nathan N <Nitharios@users.noreply.github.com>
2017-09-22 19:00:06 -1000

    All functionality working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 // jshint esversion:6
 console.log('Sanity Check: The Async Force Episode 1');
 
+
+
+getInstanceNames('http://swapi.co/api/people/4/', 'person4Name', 'name');
+getInstanceNames('http://swapi.co/api/people/4/', 'person4HomeWorld', 'homeworld');
+getInstanceNames('http://swapi.co/api/people/14/', 'person14Name', 'name');
+getInstanceNames('http://swapi.co/api/people/14/', 'person14Species', 'species');
+
+let filmList = document.getElementById('filmList');
+getMovieData('http://swapi.co/api/films/', 'filmList', 'title', 'planets');
+
 function getInstanceNames (url, id, trait) {
   let currentId = document.getElementById(id);
   let oReq = new XMLHttpRequest();
@@ -9,9 +19,9 @@ function getInstanceNames (url, id, trait) {
   oReq.send();
 }
 
+
 function reqListener (id, currentId, trait) {
   return function() { // this is the event handler
-
     let parsedDocument = JSON.parse(this.responseText);
     let urlRE = /^http/;
     if (urlRE.test(parsedDocument[trait])) {
@@ -20,32 +30,54 @@ function reqListener (id, currentId, trait) {
   };
 }
 
-getInstanceNames('http://swapi.co/api/people/4/', 'person4Name', 'name');
-getInstanceNames('http://swapi.co/api/people/4/', 'person4HomeWorld', 'homeworld');
-getInstanceNames('http://swapi.co/api/people/14/', 'person14Name', 'name');
-getInstanceNames('http://swapi.co/api/people/14/', 'person14Species', 'species');
-
-
-function generateLists(url, id, traitOne, traitTwo) {
- let currentId = document.getElementById(id);
- let oReq = new XMLHttpRequest();
- oReq.addEventListener('load', reqListListener(id, currentId, traitOne, traitTwo));
- oReq.open("GET", url);  
- oReq.send();
+function getMovieData(url, id, traitOne, traitTwo) {
+  let currentId = document.getElementById(id);
+  let oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', movieListListener(id, currentId, traitOne, traitTwo));
+  oReq.open("GET", url);  
+  oReq.send();
 }
 
-function reqListListener (id, currentId, traitOne, traitTwo) {
+function movieListListener (id, currentId, traitOne, traitTwo) {
   return function() { // this is the event handler
-    let films = JSON.parse(this.responseText).results;
+    let filmData = JSON.parse(this.responseText).results;
     let urlRE = /^http/;
     
-    for (let i = 0; i < films.length; i++) {
+    for (let i = 0; i < filmData.length; i++) {
       let filmBullet = document.createElement('li');
-      filmBullet.innerHTML = films[i][traitOne];
+      filmBullet.className = traitOne;
+      filmBullet.innerHTML = filmData[i][traitOne];
       filmList.appendChild(filmBullet);
+      
+      let filmPlanets = document.createElement('ul');
+      filmPlanets.className = 'filmPlanets';
+      filmBullet.appendChild(filmPlanets);
+
+      let traitTwoList = filmData[i][traitTwo];
+
+      if (traitTwo) {
+        for (let j = 0; j < traitTwoList.length; j++) {
+          getMovieNames(traitTwoList[j], filmPlanets, 'name');
+        }
+      }
     }
   };
 }
 
-let filmList = document.getElementById('filmList');
-generateLists('http://swapi.co/api/films/', 'filmList', 'title', 'planets');
+function getMovieNames (url, newClass, trait) {
+  let currentClass = document.getElementsByClassName(newClass);
+  let oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', planetNames(newClass, currentClass, trait));
+  oReq.open("GET", url);
+  oReq.send(); 
+}
+
+function planetNames (newClass, currentClass, trait) {
+  return function() {
+    let parsedDocument = JSON.parse(this.responseText);
+    let planetBullet = document.createElement('li');
+    planetBullet.className = 'planetName';
+    planetBullet.innerHTML = parsedDocument[trait];
+    newClass.appendChild(planetBullet);
+  };
+}
