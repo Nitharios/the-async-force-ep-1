baf1882b6f82377f0fcb86d801e02a7d3c419574
Nathan N <Nitharios@users.noreply.github.com>
2017-09-22 19:55:46 -1000

    Included comments for readability...for you Jesse

--- js/app.js
+++ js/app.js
@@ -9,11 +9,17 @@ getCharacterNames('http://swapi.co/api/people/14/', 'person14Species', 'species'
 let filmList = document.getElementById('filmList');
 getMovieData('http://swapi.co/api/films/', 'filmList', 'title', 'planets');
 
+// First function called that takes in the URL, id of DOM element to affect, and the trait the user wants returned
 function getCharacterNames(url, id, trait) {
+  // grabs all the elements with the same id
   let currentId = document.getElementById(id);
+  // calls a new instance of XMLHttpRequest
   let oReq = new XMLHttpRequest();
+  // adds an event listener that will invoke a function on page load
   oReq.addEventListener('load', reqListener(id, currentId, trait));
+  // invokes the GET on the url passed in
   oReq.open("GET", url);
+  // does something...
   oReq.send();
 }
 
@@ -21,12 +27,14 @@ function reqListener(id, currentId, trait) {
   return function() { // this is the event handler
     let parsedDocument = JSON.parse(this.responseText);
     let urlRE = /^http/;
+    // tests if the document returned contains an http link at a specific trait
     if (urlRE.test(parsedDocument[trait])) 
+      // if true, will re-run character names --> recursion 
       return getCharacterNames(parsedDocument[trait], id, 'name');
     else currentId.innerHTML = parsedDocument[trait];
   };
 }
-
+// First function called that takes in a url, id of DOM element to affect, and the two traits the user wants to inspect 
 function getMovieData(url, id, traitOne, traitTwo) {
   let currentId = document.getElementById(id);
   let oReq = new XMLHttpRequest();
@@ -34,7 +42,7 @@ function getMovieData(url, id, traitOne, traitTwo) {
   oReq.open("GET", url);  
   oReq.send();
 }
-
+// Second function called that takes in id of DOM element to affect, the first trait to build, and the second trait to build under the first trait
 function movieListListener(id, currentId, traitOne, traitTwo) {
   return function() { // this is the event handler
     let filmData = JSON.parse(this.responseText).results;
@@ -43,9 +51,11 @@ function movieListListener(id, currentId, traitOne, traitTwo) {
     for (let i = 0; i < filmData.length; i++) {
       let filmBullet = document.createElement('li');
       filmBullet.className = traitOne;
+      // fills in the HTML section of the ith iteration of traitOne
       filmBullet.innerHTML = filmData[i][traitOne];
+      // appens new element to parent element
       filmList.appendChild(filmBullet);
-
+      // if traitTwo is X
       if (traitTwo === 'planets') {
         let filmPlanets = document.createElement('ul');
         filmPlanets.className = 'filmPlanets';
@@ -54,20 +64,20 @@ function movieListListener(id, currentId, traitOne, traitTwo) {
         let traitTwoList = filmData[i][traitTwo];
         
         for (let j = 0; j < traitTwoList.length; j++) {
-          getMovieNames(traitTwoList[j], filmPlanets, 'name');
+          getPlanets(traitTwoList[j], filmPlanets, 'name');
         }
       }
     }
   };
 }
-
-function getMovieNames(url, currentClass, trait) {
+// gets the info for each planet url
+function getPlanets(url, currentClass, trait) {
   let oReq = new XMLHttpRequest();
   oReq.addEventListener('load', getPlanetNames(currentClass, trait));
   oReq.open("GET", url);
   oReq.send(); 
 }
-
+// accesses the info for each planet url, specifically the names
 function getPlanetNames(currentClass, trait) {
   return function() {
     let parsedDocument = JSON.parse(this.responseText);
