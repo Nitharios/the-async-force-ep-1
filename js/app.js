929b930e2f6ea65dfa2dceabc45f45ebd806d601
Nathan N <Nitharios@users.noreply.github.com>
2017-09-22 16:19:24 -1000

    Added listener for films test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // jshint esversion:6
 console.log('Sanity Check: The Async Force Episode 1');
 
-function openTheSource (url, id, trait) {
+function getInstanceNames (url, id, trait) {
   let currentId = document.getElementById(id);
   let oReq = new XMLHttpRequest();
   oReq.addEventListener('load', reqListener(id, currentId, trait));
@@ -13,37 +13,41 @@ function reqListener (id, currentId, trait) {
   return function() { // this is the event handler
 
     let parsedDocument = JSON.parse(this.responseText);
-    console.log(parsedDocument[trait]);
     let urlRE = /^http/;
     if (urlRE.test(parsedDocument[trait])) {
-      return openTheSource(parsedDocument[trait], id, 'name');
+      return getInstanceNames(parsedDocument[trait], id, 'name');
     } else currentId.innerHTML = parsedDocument[trait];
   };
 }
 
-openTheSource('http://swapi.co/api/people/4/', 'person4Name', 'name');
-openTheSource('http://swapi.co/api/people/4/', 'person4HomeWorld', 'homeworld');
-openTheSource('http://swapi.co/api/people/14/', 'person14Name', 'name');
-openTheSource('http://swapi.co/api/people/14/', 'person14Species', 'species');
+getInstanceNames('http://swapi.co/api/people/4/', 'person4Name', 'name');
+getInstanceNames('http://swapi.co/api/people/4/', 'person4HomeWorld', 'homeworld');
+getInstanceNames('http://swapi.co/api/people/14/', 'person14Name', 'name');
+getInstanceNames('http://swapi.co/api/people/14/', 'person14Species', 'species');
 
+function generateLists(url, id, trait) {
+ let currentId = document.getElementById(id);
+ let oReq = new XMLHttpRequest();
+ oReq.addEventListener('load', reqListListener(id, currentId, trait));
+ oReq.open("GET", url);  
+ oReq.send();
+}
 
+function reqListListener (id, currentId, trait) {
+  return function() { // this is the event handler
+    let parsedDocument = JSON.parse(this.responseText);
+    parsedDocument = parsedDocument.results;
+    console.log(parsedDocument);
+    let urlRE = /^http/;
+    if (urlRE.test(parsedDocument[trait])) {
+      return getInstanceNames(parsedDocument[trait], id, 'title');
+    } else {
+      let titleList = parsedDocument[trait];
+      for (let i = 0; i < titleList.length; i++) {
+        currentId.innerHTML = parsedDocument[trait];
+      }
+    }
+  };
+}
 
-
-
-// function reqOneListener() {
-//   let personFourParsed = JSON.parse(this.responseText);
-//   personFourName.innerHTML = personFourParsed.name;
-// }
-
-// function reqTwoListener(){
-//   let personFourteenParsed = JSON.parse(this.responseText);
-//   personFourteenName.innerHTML = personFourteenParsed.name;
-// }
-
-// oReqOne.addEventListener('load', reqOneListener);
-// oReqOne.open("GET", "http://swapi.co/api/people/4/");
-// oReqOne.send();
-
-// oReqTwo.addEventListener('load', reqTwoListener);
-// oReqTwo.open("GET", "http://swapi.co/api/people/14/");
-// oReqTwo.send();
+generateLists('http://swapi.co/api/films/', 'filmList', 'title');
